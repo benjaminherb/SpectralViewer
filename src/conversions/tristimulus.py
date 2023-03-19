c4b21d755479ec97e42c8ce97f1105635e596a50
Benjamin Herb <github@benjaminherb.de>
2023-03-19 21:05:09 +0100

    sped up XYZ_to_RGB conversion

--- src/conversions/tristimulus.py
+++ src/conversions/tristimulus.py
@@ -6,14 +6,10 @@ from src.load.load_primaries import load_primaries
 def XYZ_to_RGB(XYZ_image):
     primaries = load_primaries("sRGB")
     XYZ_to_RGB_matrix = get_XYZ_to_RGB_matrix(primaries)
-    # RGB_image = np.apply_along_axis(XYZ_image.reshape(-1, 3) *  XYZ_to_RGB_matrix
-    print(XYZ_to_RGB_matrix)
-    RGB_image = np.apply_along_axis(lambda v: np.dot(v, XYZ_to_RGB_matrix.T), axis=2,
-                                    arr=XYZ_image)
-    return RGB_image.reshape(XYZ_image.shape)
+    RGB_image = np.dot(XYZ_image, XYZ_to_RGB_matrix.T)
+    return RGB_image
 
 
 def linear_to_sRGB(v):
-    np.seterr('raise')
     return ((v > 0.0031308) * (1.055 * np.power(v, (1 / 2.4)) - 0.055)
             + (v <= 0.0031308) * (v * 12.92))
