47f4793782e97e1bf2bd9def3a38413059c75d52
Benjamin Herb <github@benjaminherb.de>
2023-03-26 12:42:37 +0200

    Updated picker tab

--- src/tabs/picker_tab.py
+++ src/tabs/picker_tab.py
@@ -9,11 +9,21 @@ class PickerTab(QtWidgets.QWidget):
         self.graph_pen = pg.mkPen('k', width=1)
 
         pg.setConfigOption('foreground', 'k')
+        # evil bad workaround as the color is not defined in its palette
+        pg.setConfigOption('background', (251, 251, 251, 255))
+
         self.plot_widget = pg.PlotWidget()
+        self.plot_widget.plotItem.setMouseEnabled(x=False)  # Only allow zoom in Y-axis
+        self.plot_widget.setYRange(0, 1)
+        self.plot_widget.plotItem.getViewBox().setLimits(yMin=0, yMax=1)  # limit zoom range
+
+        self.plot_widget.plotItem.setMenuEnabled(False)
+        self.plot_widget.plotItem.setLabel(axis='left', text='Intensity [0,1]')
+        self.plot_widget.plotItem.setLabel(axis='bottom', text='Wavelength [nm]')
+
         # self.plot_widget.setYRange(0, 1)
         self.plot_widget.plot(np.arange(0, 31) * 10 + 400, np.zeros(31))
-        self.plot_widget.setBackground(
-            QtWidgets.QMainWindow().palette().color(QtGui.QPalette.ColorRole.Base))
+        self.plot_widget.setBackground("default")
 
         self.position_label = QtWidgets.QLabel()
         layout = QtWidgets.QVBoxLayout()
@@ -30,6 +40,7 @@ class PickerTab(QtWidgets.QWidget):
             self.plot_widget.plot(
                 np.arange(0, 31) * 10 + 400, spectral_pixel_values,
                 pen=self.graph_pen)
+            self.plot_widget.setYRange(0, 1)
 
     def show_position(self, position):
         x, y = position
