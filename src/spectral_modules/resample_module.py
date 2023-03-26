beb64896dba2cf2e42fa80b94a249d5910a677b0
Benjamin Herb <github@benjaminherb.de>
2023-03-26 23:39:07 +0200

    Updated resampling

--- src/spectral_modules/resample_module.py
+++ src/spectral_modules/resample_module.py
@@ -1,4 +1,6 @@
 from PyQt6 import QtWidgets, QtGui
+import scipy
+import numpy as np
 
 
 class SpectralResampleModule(QtWidgets.QWidget):
@@ -13,7 +15,11 @@ class SpectralResampleModule(QtWidgets.QWidget):
         self.label_01 = QtWidgets.QLabel("Resample image to ")
         self.output_band_count = QtWidgets.QSpinBox()
         self.output_band_count.setValue(6)
-        self.label_02 = QtWidgets.QLabel("bands")
+        self.label_02 = QtWidgets.QLabel("bands using")
+        self.interpolation_selector = QtWidgets.QComboBox()
+        self.interpolation_selector.addItems(
+            ['linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic', 'previous', 'next'])
+        self.label_03 = QtWidgets.QLabel("interpolation")
 
         self.up_button = QtWidgets.QPushButton("Up")
         self.down_button = QtWidgets.QPushButton("Down")
@@ -23,6 +29,9 @@ class SpectralResampleModule(QtWidgets.QWidget):
         self.layout.addWidget(self.label_01)
         self.layout.addWidget(self.output_band_count)
         self.layout.addWidget(self.label_02)
+        self.layout.addWidget(self.interpolation_selector)
+        self.layout.addWidget(self.label_03)
+
         self.layout.addStretch()
         self.layout.addWidget(self.up_button)
         self.layout.addWidget(self.down_button)
@@ -31,5 +40,17 @@ class SpectralResampleModule(QtWidgets.QWidget):
 
     def process(self, image):
         output_bands = self.output_band_count.value()
-        print(output_bands)
+
+        # Reshape the original array into a 2D array
+        height, width, depth = image.data.shape
+        resampled_wavelengths = np.linspace(
+            image.minimum_wavelength, image.maximum_wavelength, output_bands)
+
+        # Create a 1D interpolation function for each row of the reshaped array
+        interpolation_function_array = scipy.interpolate.interp1d(
+            image.get_wavelengths(), image.data, kind=self.interpolation_selector.currentText(), axis=2)
+
+        # Use the interpolation function to interpolate to the new axis values
+        image.data = interpolation_function_array(resampled_wavelengths)
+
         return image
