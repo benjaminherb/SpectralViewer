6fce3ff01c749ea1a05b4de2ab600a73c930cd31
Benjamin Herb <github@benjaminherb.de>
2023-04-06 01:56:05 +0200

    Added button for characterization matrix

--- src/spectral_to_rgb_modules/camera_conversion_module.py
+++ src/spectral_to_rgb_modules/camera_conversion_module.py
@@ -13,19 +13,25 @@ class CameraConversionModule(QtWidgets.QWidget):
         super().__init__()
 
         # Observer
-        observer_label = QtWidgets.QLabel("Camera:")
+        observer_label = QtWidgets.QLabel("Camera Response:")
         self.camera_selector = QtWidgets.QComboBox()
         self.camera_selector.addItems(get_camera_names())
+        self.characterisation_label = QtWidgets.QLabel("Characterization Matrix:")
+        self.characterisation_checkbox = QtWidgets.QCheckBox("Apply")
 
         layout = QtWidgets.QGridLayout()
         layout.addWidget(observer_label, 0, 0)
         layout.addWidget(self.camera_selector, 0, 1)
+        layout.addWidget(self.characterisation_label, 1, 0)
+        layout.addWidget(self.characterisation_checkbox, 1, 1)
+        layout.setAlignment(self.characterisation_checkbox, QtCore.Qt.AlignmentFlag.AlignHCenter)
         layout.setColumnStretch(layout.columnCount(), 1)
         self.setLayout(layout)
 
     def process(self, spectral_image):
         image = spectral_to_RGB_using_camera_response(
             spectral_image, self.camera_selector.currentText(), 10)
-        matrix = get_camera_characterisation_matrix(self.camera_selector.currentText())
-        image = np.dot(image, matrix)
+        if self.characterisation_checkbox.isChecked():
+            matrix = get_camera_characterisation_matrix(self.camera_selector.currentText())
+            image = np.dot(image, matrix)
         return image
